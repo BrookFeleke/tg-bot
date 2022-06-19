f30695487991f3d3d55054de83d68ec2d3086ea9
BrookFeleke <felekebrook05@gmail.com>
2022-06-19 12:51:34 +0300

    sends dad jokes

--- index.js
+++ index.js
@@ -1,50 +1,103 @@
 const TelegramBot = require("node-telegram-bot-api");
-const http = require('http')
+var cron = require("node-cron");
+
+const axios = require("axios");
+var events = require("events");
+  
 require("dotenv").config();
 const token = process.env.BOT_TOKEN;
-console.log(token)
+console.log(token);
 const bot = new TelegramBot(token, { polling: true });
 const channelID = process.env.CHANEL_ID;
 const nati = process.env.GROUP_ID;
-console.log(process.env.BOT_TOKEN);
-const r = new RegExp(/^[0-9()]*( ?[+\-*/=]{1}[0-9()]+)*?[+\-*/=]?[0-9()]$/gm);
-bot.on("message", (msg) => {
-  if (msg.text.toString().toLowerCase().includes("post")) {
-    {
-      bot.sendMessage(
-        nati,
-        msg.text.replace("post", `${msg.chat.first_name} sent: `)
-      );
-    }
-  }
+const myID = process.env.MY_ID;
+const edilID = process.env.EDIL_ID;
 
-  bot.sendMessage(msg.chat.id, ` ${msg.chat.id}`);
-  if (msg.forward_from_chat) {
-    bot.sendMessage(
-      msg.chat.id,
-      `forwarded fron chat ${msg.forward_from_chat.id}`
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+const type = ["top", "hot", "new"];
+const time = ["day", "week", "year"];
+async function getting() {
+  // console.log(getRandom(0,type.length))
+  const randomTime = time[getRandom(0, time.length)];
+  const randomType = type[getRandom(0, type.length)];
+  console.log(randomTime, "--------------------");
+  let response;
+  await axios
+    .get(`https://www.reddit.com/r/dadjokes/top.json?t=${randomTime}`)
+    .then((result) => {
+      response = result;
+      // console.log("This is the full data---------", result);
+      // console.log("This is the data------------- ",result["data"]);
+      // console.log("this is the children---------", result["data"]["data"]);
+    })
+    .catch(console.log("there was an error"));
+
+  for (let i = 0; i < 3; i++) {
+    let randomPost = getRandom(0, response["data"]["data"]["children"].length);
+    console.log(
+      "this is the child---------",
+      response["data"]["data"]["children"].length
     );
+    console.log(
+      "this is the child---------",
+      response["data"]["data"]["children"][randomPost]["data"]["title"]
+    );
+    console.log(
+      "this is the child---------",
+      response["data"]["data"]["children"][randomPost]["data"]["selftext"]
+    );
+    const joke =
+      response["data"]["data"]["children"][randomPost]["data"]["title"];
+    const punchLine =
+      response["data"]["data"]["children"][randomPost]["data"]["selftext"];
+    bot.sendMessage(channelID, `${joke}  \n\n ${punchLine}`);
+    bot.sendMessage(myID, `${joke}  \n\n ${punchLine}`);
   }
-  if (r.test(msg.text)) {
-    bot.sendMessage(
-      msg.chat.id,
-      `Yes this is a math equation good job  \n (if you are confused and dont think that it should be a math equation then constants could be math equations iko )`
-    );
-  } else {
-    bot.sendMessage(
-      msg.chat.id,
-      "This is not a math equation what were u thinking "
-    );
+}
+
+bot.on("message", (msg) => {
+  if (
+    msg.text.toString().toLowerCase().includes("post") &&
+    msg.from.id === parseInt(myID)
+  ) {
+    console.log(myID);
+    console.log(msg.from.id);
+    console.log(msg);
+    getting();
   }
-  console.log(msg);
-  console.log("----------------------------");
+  if(
+    msg.text.toString().toLowerCase().includes("pill") &&
+    msg.from.id === parseInt(edilID)
+  ){
+    bot.sendMessage(myID, `Dont forget to take ur pills. \n\n XX`);
+    bot.sendMessage(edilID, `Dont forget to take ur pills. \n\n XX`);
+  }
 });
 
-const PORT = process.env.PORT || 3000
+cron.schedule("23 * * * *", () => {
+  console.log("running a task every three minutes");
+  bot.emit("message", {
+    message_id: 261,
+    from: {
+      id: parseInt(myID),
+      is_bot: false,
+      first_name: "Brook",
+      username: "B_r_o_o_k19",
+      language_code: "en",
+    },
+    chat: {
+      id: parseInt(myID),
+      first_name: "Brook",
+      username: "B_r_o_o_k19",
+      type: "private",
+    },
+    date: 1655547586,
+    text: "Post",
+  });
+});
 
-http.createServer((req,res)=>{
-  if (req.url === "/") {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h1>Hello</h1>");
-  }
-}).listen(PORT)
+cron.schedule("00 19 * * *", () => {
+  bot.emit("message", { text: "pill", from: { id: parseInt(edilID) } });
+});
