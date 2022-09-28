d1ce0f8ecbced49d5cfe262fb76ad42123b78318
BrookFeleke <felekebrook05@gmail.com>
2022-09-28 15:28:04 +0300

    feat: send dad jokes

--- api/webhook.js
+++ api/webhook.js
@@ -1,48 +1,95 @@
 // https://github.com/yagop/node-telegram-bot-api/issues/319#issuecomment-324963294
 // Fixes an error with Promise cancellation
-process.env.NTBA_FIX_319 = 'test';
-
+const axios = require("axios");
+process.env.NTBA_FIX_319 = "test";
+const bot = new TelegramBot(token, { polling: true });
+const channelID = process.env.CHANEL_ID;
+const myID = process.env.MY_ID;
 // Require our Telegram helper package
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+const type = ["top", "hot", "new"];
+const time = ["day", "week", "year"];
+
+async function getting() {
+  const randomTime = time[getRandom(0, time.length)];
+  const randomType = type[getRandom(0, type.length)];
+  console.log(randomTime, "--------------------");
+  let response;
+  try {
+    await axios
+      .get(`https://www.reddit.com/r/dadjokes/top.json?t=${randomTime}`)
+      .then((result) => {
+        response = result;
+      })
+      .catch(console.log("there was an error"));
+
+    for (let i = 0; i < 3; i++) {
+      let randomPost = getRandom(
+        0,
+        response["data"]["data"]["children"].length
+      );
+      const joke =
+        response["data"]["data"]["children"][randomPost]["data"]["title"];
+      const punchLine =
+        response["data"]["data"]["children"][randomPost]["data"]["selftext"];
+      await bot.sendMessage(channelID, `${joke}  \n\n ${punchLine}`);
+      await bot.sendMessage(myID, `${joke}  \n\n ${punchLine}`);
+    }
+  } catch (error) {
+    console.error("Error sending message");
+    console.log(error.toString());
+  }
+}
 
 // Export as an asynchronous function
 // We'll wait until we've responded to the user
 module.exports = async (request, response) => {
-    try {
-        // Create our new bot handler with the token
-        // that the Botfather gave us
-        // Use an environment variable so we don't expose it in our code
-        const bot = new TelegramBot(process.env.BOT_TOKEN);
-        // Retrieve the POST request body that gets sent from Telegram
-        const { body } = request;
-        console.log(body);
+  try {
+    // Create our new bot handler with the token
+    // that the Botfather gave us
+    // Use an environment variable so we don't expose it in our code
+    // const bot = new TelegramBot(process.env.BOT_TOKEN);
+    // Retrieve the POST request body that gets sent from Telegram
+    const { body } = request;
+    console.log(body);
 
-        // Ensure that this is a message being sent
-        if (body.message) {
-            // Retrieve the ID for this chat
-            // and the text that the user sent
-            const { chat: { id }, text } = body.message;
+    // Ensure that this is a message being sent
+    if (body.message) {
+      // Retrieve the ID for this chat
+      // and the text that the user sent
+      const {
+        chat: { id },
+        text,
+      } = body.message;
 
-            // Create a message to send back
-            // We can use Markdown inside this
-            const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+      if (
+        message.text.toString().toLowerCase().includes("post") &&
+        message.from.id === parseInt(myID)
+      ) {
+        getting();
+      } else {
+        // Create a message to send back
+        // We can use Markdown inside this
+        const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
 
-            // Send our new message back in Markdown and
-            // wait for the request to finish
-            await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
-        }
+        // Send our new message back in Markdown and
+        // wait for the request to finish
+        await bot.sendMessage(id, message, { parse_mode: "Markdown" });
+      }
     }
-    catch(error) {
-        // If there was an error sending our message then we 
-        // can log it into the Vercel console
-        console.error('Error sending message');
-        console.log(error.toString());
-    }
-    
-    // Acknowledge the message with Telegram
-    // by sending a 200 HTTP status code
-    // The message here doesn't matter.
-    response.send('OK');
+  } catch (error) {
+    // If there was an error sending our message then we
+    // can log it into the Vercel console
+    console.error("Error sending message");
+    console.log(error.toString());
+  }
+
+  // Acknowledge the message with Telegram
+  // by sending a 200 HTTP status code
+  // The message here doesn't matter.
+  response.send("OK");
 };
-
-
--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-// require("dotenv").config();
-// const TelegramBot = require("node-telegram-bot-api");
-// var cron = require("node-cron");
-// const axios = require("axios");
-// const port = 8889;
-// const express = require("express");
-// const app = express();
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+var cron = require("node-cron");
+const axios = require("axios");
+const port = 8889;
+const express = require("express");
+const app = express();
 
-// const token = process.env.BOT_TOKEN;
-// console.log(token);
-// const bot = new TelegramBot(token, { polling: true });
-// const channelID = process.env.CHANEL_ID;
-// const myID = process.env.MY_ID;
+const token = process.env.BOT_TOKEN;
+console.log(token);
+const bot = new TelegramBot(token, { polling: true });
+const channelID = process.env.CHANEL_ID;
+const myID = process.env.MY_ID;
 
-// function getRandom(min, max) {
-//   return Math.floor(Math.random() * (max - min) + min);
-// }
-// const type = ["top", "hot", "new"];
-// const time = ["day", "week", "year"];
-// async function getting() {
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+const type = ["top", "hot", "new"];
+const time = ["day", "week", "year"];
+async function getting() {
 
-//   const randomTime = time[getRandom(0, time.length)];
-//   const randomType = type[getRandom(0, type.length)];
-//   console.log(randomTime, "--------------------");
-//   let response;
-//   await axios
-//     .get(`https://www.reddit.com/r/dadjokes/top.json?t=${randomTime}`)
-//     .then((result) => {
-//       response = result;
-//     })
-//     .catch(console.log("there was an error"));
+  const randomTime = time[getRandom(0, time.length)];
+  const randomType = type[getRandom(0, type.length)];
+  console.log(randomTime, "--------------------");
+  let response;
+  await axios
+    .get(`https://www.reddit.com/r/dadjokes/top.json?t=${randomTime}`)
+    .then((result) => {
+      response = result;
+    })
+    .catch(console.log("there was an error"));
 
-//   for (let i = 0; i < 3; i++) {
-//     let randomPost = getRandom(0, response["data"]["data"]["children"].length)
-//     const joke =
-//       response["data"]["data"]["children"][randomPost]["data"]["title"];
-//     const punchLine =
-//       response["data"]["data"]["children"][randomPost]["data"]["selftext"];
-//     bot.sendMessage(channelID, `${joke}  \n\n ${punchLine}`);
-//     bot.sendMessage(myID, `${joke}  \n\n ${punchLine}`);
-//   }
-// }
+  for (let i = 0; i < 3; i++) {
+    let randomPost = getRandom(0, response["data"]["data"]["children"].length)
+    const joke =
+      response["data"]["data"]["children"][randomPost]["data"]["title"];
+    const punchLine =
+      response["data"]["data"]["children"][randomPost]["data"]["selftext"];
+    bot.sendMessage(channelID, `${joke}  \n\n ${punchLine}`);
+    bot.sendMessage(myID, `${joke}  \n\n ${punchLine}`);
+  }
+}
 
-// bot.on("message", (msg) => {
-//   console.log(msg);
-//   if (
-//     msg.text.toString().toLowerCase().includes("post") &&
-//     msg.from.id === parseInt(myID)
-//   ) {
-//     getting();
-//   }
-// });
+bot.on("message", (msg) => {
+  console.log(msg);
+  if (
+    msg.text.toString().toLowerCase().includes("post") &&
+    msg.from.id === parseInt(myID)
+  ) {
+    getting();
+  }
+});
 
-// // Emmits message event according to schedule
-// cron.schedule("*/10 * * * *", () => {
-//   console.log("running a task every three minutes");
-//   bot.emit("message", {
-//     message_id: 261,
-//     from: {
-//       id: parseInt(myID),
-//     },
-//     chat: {
-//       id: parseInt(myID),
-//     },
-//     text: "Post",
-//   });
-// });
+// Emmits message event according to schedule
+cron.schedule("*/10 * * * *", () => {
+  console.log("running a task every three minutes");
+  bot.emit("message", {
+    message_id: 261,
+    from: {
+      id: parseInt(myID),
+    },
+    chat: {
+      id: parseInt(myID),
+    },
+    text: "Post",
+  });
+});
 
-// app.get('/',(req,res)=>{
-//   res.json({message: "Yes this is working"});
-// })
+app.get('/',(req,res)=>{
+  res.json({message: "Yes this is working"});
+})
 
 
-// app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || port);
