873792b0001c82c2a2daf98317554751aed05058
BrookFeleke <felekebrook05@gmail.com>
2022-10-08 22:48:40 +0300

    fix: fixed bug

--- api/webhook.js
+++ api/webhook.js
@@ -18,7 +18,8 @@ const time = ["day", "week", "year"];
 const getDefinition = async (word)=> {
   try {
     const result = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    const b = await result.json()
+    // const b = await result.json()
+    const b = await result.data
     console.log(b)
     const firstDef = await b[0];
     const resultword = await firstDef.word;
